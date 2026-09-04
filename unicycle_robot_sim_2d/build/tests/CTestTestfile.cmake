34fef7af0b05e7702917df918b22d2cc8e4a7245
Cason <casonkchance@gmail.com>
2026-09-04 12:27:51 -0500

    Fix: permanently remove .vscode and build artifacts from tracking

--- unicycle_robot_sim_2d/build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cason/robotics_portfolio/differential_drive_robot_sim_2d/tests
-# Build directory: /home/cason/robotics_portfolio/differential_drive_robot_sim_2d/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[RunRobotTests]=] "/home/cason/robotics_portfolio/differential_drive_robot_sim_2d/build/tests/TestRobot")
-set_tests_properties([=[RunRobotTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/cason/robotics_portfolio/differential_drive_robot_sim_2d/tests/CMakeLists.txt;10;add_test;/home/cason/robotics_portfolio/differential_drive_robot_sim_2d/tests/CMakeLists.txt;0;")
