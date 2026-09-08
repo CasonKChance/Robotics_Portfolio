d897484d3e349dbd7770bf8b98ac45a29a22b95e
Cason <casonkchance@gmail.com>
2026-09-07 20:40:00 -0500

    Use a trajectory input file to control the robot

--- unicycle_robot_sim_2d/src/Main.cpp
+++ unicycle_robot_sim_2d/src/Main.cpp
@@ -4,27 +4,48 @@
 
 #include <numbers>
 #include <iostream>
+#include <fstream>
+#include <filesystem>
+
+std::string getTrajectoryInputPath() {
+    std::filesystem::path logPath = std::filesystem::path(PROJECT_SOURCE_DIR) / "input" / "Trajectory.csv";
+    return logPath.string();
+}
 
 int main() {
     Robot robot({1.0, 1.0, 0.0});
-    World  world(10, 10, {}, Obstacle{5.0, 5.0, 1.0});
+    World  world(10, 10, {{2.0, 4.0, 1.0}, {4.0, 2.0, 1.0}, {6.0, 6.0, 1.75}}, Obstacle{8.0, 8.0, 1.0});
 
     Simulator simulator = Simulator(robot, world, 0.01);
 
-    while (simulator.getStatus() == SimulationStatus::Running) {
-        robot.setVelocityCommand({
-            .linearVelocity = 0.0,
-            .angularVelocity = std::numbers::pi / 4.0
-        });
+    std::string trajectoryInputFilePath{ getTrajectoryInputPath() };
+    std::ifstream trajectoryInputFile(trajectoryInputFilePath, std::ios::in);
+    if (!trajectoryInputFile.is_open()) {
+        throw std::runtime_error("Error: Could not open robot data log file: " + trajectoryInputFilePath);
+    }
 
-        simulator.runFor(1.0);
+    std::string line;
+    std::getline(trajectoryInputFile, line); // Skip the header line
 
-        robot.setVelocityCommand({
-            .linearVelocity = 1.0,
-            .angularVelocity = 0.0
-        });
+    while (simulator.getStatus() == SimulationStatus::Running && std::getline(trajectoryInputFile, line)) {
+        std::stringstream ss(line);
+        std::string linearStr, angularStr, timeStr;
 
-        simulator.runFor(5);
+        if (std::getline(ss, linearStr, ',') &&
+            std::getline(ss, angularStr, ',') &&
+            std::getline(ss, timeStr, ',')) {
+
+            double linearVelocity = std::stod(linearStr);
+            double angularVelocity = std::stod(angularStr);
+            double time = std::stod(timeStr);
+
+            robot.setVelocityCommand({
+                .linearVelocity = linearVelocity,
+                .angularVelocity = angularVelocity
+            });
+
+            simulator.runFor(time);
+        }
     }
 
     switch(simulator.getStatus()) {
